c74c846f4225542ad19c61057430aa7ca6f02206
Elias700 <elias_ribeiro07@outlook.com.br>
2025-12-19 13:13:01 -0300

    alteracao de titulo do usuario

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div style={{ padding: "20px" }}>
-      <h1>Cadastro de Usuários</h1>
+      <h1>Cadastro de Usuários Cadastrados</h1>
 
       <form>
         <div>
