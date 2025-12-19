1a244444f9994795cdb235082725f612b2cf9f78
Elias700 <elias_ribeiro07@outlook.com.br>
2025-12-19 15:03:14 -0300

    Adicionando component Login

---
+++ src/components/Login.tsx
@@ -0,0 +1,40 @@
+
+import { useState } from "react";
+
+const Login = () => {
+
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+
+    return (
+         <div style={{ padding: "20px" }}>
+      <h1>Cadastro</h1>
+
+      <form>
+        <div>
+          <label>Nome:</label><br />
+          <input
+            type="text"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+        </div>
+
+        <div style={{ marginTop: "10px" }}>
+          <label>Email:</label><br />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <button style={{ marginTop: "15px" }}>
+          Cadastrar
+        </button>
+      </form>
+    </div>
+    )
+};
+
+export default Login;
